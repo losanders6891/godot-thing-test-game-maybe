2e34b11d534446bcf41f57ef54af47ae0bb5ac00
losanders6891 <losanders6891@student.pps.net>
2025-09-07 10:12:53 -0700

    more move

--- bean.gd
+++ bean.gd
@@ -3,3 +3,24 @@ var movespeed = 7
 var fallspeed = 15
 var jumpspeed = 20
 var dashspeed = 30
+
+#start the main physics loop
+func _physics_process(delta):
+	#initialize and reset direction
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("walk-forward"):
+		direction.z -= 1
+	if Input.is_action_pressed("walk-backward"):
+		direction.z += 1
+	if Input.is_action_pressed("strafe-left"):
+		direction.x -= 1
+	if Input.is_action_pressed("strafe-right"):
+		direction.x += 1
+	#normalize movement
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+	
+	
+	
+	
