7b4d6a030ece5829920b0ed5a40f04452f4ed254
losanders6891 <losanders6891@student.pps.net>
2025-10-14 13:54:35 -0700

    E

--- skene/neck.gd
+++ skene/neck.gd
@@ -1,1 +1,19 @@
 extends Node3D
+var yLockLow = -90
+var yLockHigh = 90
+@export var _rotation: Vector3 
+
+func updateCameraRotation(input: Vector3): 
+	_rotation.x = input.y
+	_rotation.y = input.x
+	_rotation.x = clamp(_rotation.x,deg_to_rad(yLockLow),deg_to_rad(yLockHigh))
+	
+	var playerRotation = Vector3(0.0,_rotation.y,0.0)
+	var cameraRotation = Vector3(_rotation.x,0.0,0.0)
+	
+	transform.basis = basis.from_euler(cameraRotation)
+	
+	rotation.z = 0.0 
+	
+func updateCameraHeight(delta:) -> void:
+	position.y = clampf(position.y,0, 0)
