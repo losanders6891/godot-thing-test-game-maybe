c4058511fcdc4be3b82d75ea5e5f080dbe965e4b
losanders6891 <losanders6891@student.pps.net>
2025-12-03 17:03:42 -0800

    movement

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # godot-thing-test-game-maybe
-a game maybe
+a game maybe, but its not real, and never will be, so dont bother
--- scrips/player/bean.gd
+++ scrips/player/bean.gd
@@ -1,26 +1,39 @@
 class_name playerController extends CharacterBody3D
-var movespeed = 7
-var gravity = 15
+var movespeed = 14
+var gravity = 0.8
 var jumpspeed = 20 
 var dashspeed = 30
+var acceleration = 0.5
+var deceleration = 0.5
+var movementVelocity : Vector3 = Vector3.ZERO
 
+#rotates player
 func update_rotation(inputRotation) -> void:
 	global_transform.basis = Basis.from_euler(inputRotation)
 #start the main physics loop
 func _physics_process(delta):
 	#initialize and reset direction
-	var relative_direction = Vector3.ZERO
 	if Input.is_action_pressed("noLock"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	#XZ = ground plane
-	inputDir = Input.get_vector("strafe-left","strafe-right",)
+		#jumps and falls
 	if Input.is_action_pressed("jump") and  is_on_floor():
 		velocity.y += jumpspeed
 	if not is_on_floor():
 		velocity.y -= gravity
-	#normalize movement
-	if relative_direction != Vector3.ZERO:
-		relative_direction = relative_direction.normalized() 
-		velocity = relative_direction * movespeed
-		move_and_slide()
+	
+	#XZ = ground plane
+	var inputDir = Input.get_vector("strafe-left","walk-backward","walk-forward","strafe-right")
+	var currentVelocity = Vector2(movementVelocity.x, movementVelocity.z)
+	var direction = (transform.basis * Vector3(inputDir.x,0,inputDir.y)).normalized()
+	
+	if direction:
+		currentVelocity = lerp(currentVelocity,Vector2(direction.x,direction.z) * movespeed,acceleration)
+	else:
+		currentVelocity = currentVelocity.move_toward(Vector2.ZERO,deceleration)
+	
+	movementVelocity = Vector3(currentVelocity.x,velocity.y,currentVelocity.y)
+	
+	velocity = movementVelocity
+	
+	move_and_slide()
 		
