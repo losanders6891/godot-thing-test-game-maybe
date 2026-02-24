2b82ee89ad761b6426493d36c670d9fd4ebea54c
losanders6891 <losanders6891@student.pps.net>
2026-02-24 12:25:04 -0800

    started shooting

--- scrips/player/pistolcode.gd
+++ scrips/player/pistolcode.gd
@@ -1,13 +1,16 @@
 extends Node3D
-
+var projectile = preload("res://skene/projectiles/pistolbullet_test.tscn")
 ##shoots a bullet dealing 30 damage
 func shoot():
 	#TODO: create and instantiate projectile or find out how to do hitscan
+	projectile.instantiate()
+	add_child(projectile)
 
 
 
 func _process(_delta: float) -> void:
 	if (Input.is_action_just_pressed("shoot")):
 		shoot()
+		
 	
 	
